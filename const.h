93aeffbe44de04c9c646566135ed3aa341e57cc2
Luca Bretta <luca.bretta@studio.unibo.it>
2019-02-18 18:01:45 +0100

    ho rimosso ciao mamma ciao papà

--- const.h
+++ const.h
@@ -1,11 +1,11 @@
 #ifndef _CONST_H
 #define _CONST_H
 
-/********ciao papà******************************************************************** 
+/**************************************************************************** 
  *
  * This header file contains the global constant & macro definitions.
  * 
- **ciao mamma**************************************************************************/
+ ****************************************************************************/
 
 /* Maxi number of overall (eg, system, daemons, user) concurrent processes */
 #define MAXPROC 20
