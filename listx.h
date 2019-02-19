b442f5abfe1b52c79f0d08384d8cf1aaf55c8f40
Luca Bretta <luca.bretta@studio.unibo.it>
2019-02-19 16:01:50 +0100

    commenti vari

--- listx.h
+++ listx.h
@@ -50,7 +50,7 @@ typedef unsigned int    size_tt;
     La struttura list_head e' una semplice coppia di puntatore, per implementare
     una lista bidirezionale. Per creare liste di strutture arbitrarie basta
     inserire al loro interno un campo di tipo list_head. 
-*/
+*/	
 struct list_head {
 	struct list_head *next, *prev;
 };
@@ -101,9 +101,7 @@ static inline void INIT_LIST_HEAD(struct list_head *list)
     prev: elemento che deve precedere new
     next: elemento che deve seguire new
 */
-static inline void __list_add(struct list_head *new,
-		struct list_head *prev,
-		struct list_head *next)
+static inline void __list_add(struct list_head *new, struct list_head *prev, struct list_head *next)
 {
 	next->prev = new;
 	new->next = next;
