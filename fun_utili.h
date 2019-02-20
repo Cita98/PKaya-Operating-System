41a36e01c8f1daacd6e13a90e81335b5616b6e43
Cita98 <martinaaccorsi98@hotmail.it>
2019-02-20 13:59:00 +0100

    Aggiunto file per funzioni ausiliarie

---
+++ fun_utili.h
@@ -0,0 +1,19 @@
+#ifndef FUN_UTILI_H
+#define FUN_UTILI_H
+
+#include "types_rikaya.h"
+
+/**************************************************************************** 
+ *
+ * This header file contains: Funzioni utili per le varie implementazioni
+ *
+ ****************************************************************************/
+
+/******************* FUNZIONI PER PCB *************************/
+
+/* Inizializza stato */
+
+static inline struct state_t INIT_STATE( stato 
+
+
+#endif
