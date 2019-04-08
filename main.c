5e904adf72f2ab1d547bb373b271d66b2afd166c
Andriy Diachuk <113andriuscia@gmail.com>
2019-04-08 16:01:11 +0200

    aggiunto processi, da riguardare

--- main.c
+++ main.c
@@ -6,12 +6,6 @@
 #include "myConst.h"
 #include "handler.h"
 
-/* Variabili*/
-
-int processCount; //contatore dei processi 
-struct list_head readyQueue; //coda processi ready; 
-pcb_t *currentProcess[MAX_CPU]; //puntatore al processo in esecuzione
-state_t scheduler_state[MAX_CPU] //state_t dello scheduler
 
 //PROCESSI DI TEST
 extern void test1();
@@ -42,43 +36,16 @@ initPcbs();
 
 LIST_HEAD(ready_queue);
 
-
-/***********AGGIUNTA PROCESSI*/
-/* come ha fatto il ragazzo
-	//test1
-	pcb_t* p1 = allocPcb();
-	STST(&(p1->p_s));
-	p1->p_s.pc_epc = p1->p_s.reg_t9 = (memaddr)test1;
-	p1->p_s.reg_sp = PFRAMES_START;
-	p1->p_s.status = PROCESS_STATUS;
-	addReady(p1);
-	
-	
-	//test2
-	pcb_t* p2 = allocPcb();
-	STST(&(p2->p_s));
-	p2->p_s.pc_epc = p2->p_s.reg_t9 = (memaddr)test2;
-	p2->p_s.reg_sp = PFRAMES_START-FRAME_SIZE;
-	p2->p_s.status = PROCESS_STATUS;
-	addReady(p2);
-	
-	
-	//test3
-	pcb_t* p3 = allocPcb();
-	STST(&(p3->p_s));
-	p3->p_s.pc_epc = p3->p_s.reg_t9 = (memaddr)test3;
-	p3->p_s.reg_sp = PFRAMES_START-2*FRAME_SIZE;
-	p3->p_s.status = PROCESS_STATUS;
-	//~ debug(2, prova3);
-	addReady(p3);
-*/
+/*aggiungo processi*/
 
 // come farei io e come ha consigliato il tutor
 pcb_t* p1=allocPcb();
 LDST(p1->p_s); // non so se serve mettere & prima
-//poi immagino bisogni fare questo
+//poi immagino bisogni fare questo come il tipo
 p1->p_s.pc_epc = p1->p_s.reg_t9 = (memaddr)test1;
 p1->p_s.reg_sp = PFRAMES_START;  //costante non definita
 p1->p_s.status = PROCESS_STATUS; //costante non definita
+
+list_add_tail(pcb_t* p1, ready_queue* head); //da aggiustare la sintassi
 
 }
