cadc71215e368c0e539ff3f16f96b4c016bf9f86
Cita98 <martinaaccorsi98@hotmail.it>
2019-04-20 00:29:23 +0200

    Scheduler fatto, iniziate le syscall, da fare scheduler.h

--- scheduler.c
+++ scheduler.c
@@ -1,7 +1,43 @@
 #include "scheduler.h"
+#include "pcb.h"
 
-void Context_Switch()
+pcb_t* current_proc = NULL;
+
+void Scheduler(struct list_head* head_rq)
+{
+	/* Prendo il processo con priorità maggiore */
+	current_proc = removeProcQ(head_rq);
+	log_processor_order(current_proc->original_priority);
+	/* Controllo se la lista è vuota, nel caso metto in attesa */
+	if (current_proc =! NULL){
+	/* Riporto la priorità del processo a quella originaria */
+	current_proc->priority = current_proc->original_priority;
+	/* Aumento la priorità di tutti gli altri processi che restano in coda */
+	aging(head_rq);
+	/* Reimposto il timer */
+	setTIMER(TIME_SLICE);
+	/* Carico lo stato del processo all'interno del processore */
+	LDST(&(current_proc->p_s));
+	}
+	else{HALT();}
+}
+
+void Context_Switch(struct liste_head* head_rq)
 {    
-    
-    
+	if(current_proc =! NULL){
+	/* Reinserisco il processo corrente (se esiste) nella lista dei processi pronti */
+	insertProcQ(head_rq, current_proc);
+	}
+	/* Prendo il prossimo processo in lista e lo carico, richiamando la funzione dello scheduler */
+	Scheduler(head_rq);
+	
+}
+
+void aging(struct list_head* head_rq)
+{
+	/* Creo l'iteratore della lista ready queue */
+	struct list_head* iter = head_rq->next;
+	/* Scorro la lista e aumento di 1 la priorità di ogni processo */
+	list_for_each_entry(iter, head_rq, p_next)
+	{ iter->priority = iter->priority+1; }
 }
--- syscall.c
+++ syscall.c
@@ -1,1 +1,9 @@
 #include "syscall.h"
+
+void TerminateProcess(struct list_head* head_rd)
+{
+	/* Rimuovo il processo da terminare dalla ready queue */
+	pcb_t* current = removeProcQ(head_rd);
+	/* Rimuovo anche tutti i suoi figli, prima o poi lo farò */
+	
+}
