a08b1170193819e2591a43c18c4a4cd544010af5
Cita98 <martinaaccorsi98@hotmail.it>
2019-04-23 20:27:58 +0200

    Tutto funzionante, dubbi sul Makefile, documentazione fatta

--- interrupt.c
+++ interrupt.c
@@ -2,9 +2,9 @@
 
 int getINT_LINE()
 {
-	unsigned int int_line = getCAUSE(); 
-	int_line = int_line >> 2; 
-	int_line = (int_line&(0x0000001F));
+	unsigned int int_line = getCAUSE(); // Leggo il contenuto del registro cause
+	int_line = int_line >> 2; //shift a destra di 2 bit
+	int_line = (int_line&(0x0000001F)); // And bit a bit per avere un intero che identifichi la linea 
 	return int_line;
 }
 
